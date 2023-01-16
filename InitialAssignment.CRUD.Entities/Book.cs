9e5ad7fe73f472a8afbfb044f84aa21b885d3f7d
JMauricio6 <jmauriciorivera@gmail.com>
2023-01-15 19:25:11 -0600

    Creacion de clases de la capa DAL Y Entities

---
+++ InitialAssignment.CRUD.Entities/Book.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InitialAssignment.CRUD.Entities;
+
+public partial class Book
+{
+    public int Id { get; set; }
+
+    public string Author { get; set; } = null!;
+
+    public string Classification { get; set; } = null!;
+
+    public int Edition { get; set; }
+
+    public string Editorial { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public DateTime PublicationDate { get; set; }
+
+    [NotMapped]
+    public int Top_Aux { get; set; }
+}
