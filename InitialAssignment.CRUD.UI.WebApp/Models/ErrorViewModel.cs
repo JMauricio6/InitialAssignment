e3a5e619d96f2c47bf415cfe776eec0a4b7ec907
JMauricio6 <jmauriciorivera@gmail.com>
2023-01-15 18:17:30 -0600

    Creacion de la arquitectura del proyecto

---
+++ InitialAssignment.CRUD.UI.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InitialAssignment.CRUD.UI.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
